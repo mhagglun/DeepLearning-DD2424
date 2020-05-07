c660414217fed5da5abd3ab6c11ab18dd5a22168
Marcus <mhagglun@gmail.com>
2020-05-07 21:58:23 +0200

    add method for parsing tweets

---
+++ A4/data/trump_tweet_data_archive/README.md
@@ -0,0 +1,17 @@
+# [Link to the repository from which the data was obtained](https://github.com/bpb27/trump_tweet_data_archive)
+
+
+Example data from the condensed JSON files:
+
+```
+  {
+    "source": "Twitter for iPhone",
+    "id_str": "838899465390018560",
+    "text": "I am a monster-man!",
+    "created_at": "Mon Mar 06 23:49:53 +0000 2017",
+    "retweet_count": 8021,
+    "in_reply_to_user_id_str": null,
+    "favorite_count": 33632,
+    "is_retweet": false
+  }
+```
---
+++ A4/parser.py
@@ -0,0 +1,52 @@
+import sys
+import os
+import json
+import html
+import unidecode
+import re
+import numpy as np
+
+
+def read_tweets(filename, output):
+    """Parses the given json file, removing any non latin-1 character, URLs 
+    and adds padding to each line so that its 140 characters long.
+    The result is then written in lower case to the path specified by the ouput argument.
+
+    Arguments:
+        filename {string} -- The path to the json file to parse
+        output {string} -- The path to the file to write the output to
+    """    
+    output_txt = ''
+    with open (filename, 'r') as json_file:
+        tweets = json.load(json_file)
+        for tweet in tweets:
+            # Read tweet, ignoring characters which are not part of latin-1
+            txt = tweet['text'].encode('latin-1', 'ignore').decode('latin-1')
+            # Replace special characters such as â with a
+            txt = unidecode.unidecode(txt)
+            # Remove html characters like &amp
+            txt = html.unescape(txt)
+            # remove urls
+            txt = re.sub(r'http\S+', '', txt)
+            # convert all letter to lower case
+            txt = txt.lower()
+            # Add start/end of tweet character
+            # txt = '>' + txt
+            # Padd text so that its 140 characters
+            txt = txt.ljust(140)
+            output_txt += txt
+
+    with open(output, 'w') as output_file:
+        output_file.write(output_txt)
+
+def parse_all_tweets(directory='./data/trump_tweet_data_archive-master/', output='data/raw_tweets.txt'):
+    """Parses all json files in the specified directory using read_tweets() and writes the output to a .txt file
+
+    Keyword Arguments:
+        directory {str} -- [The path to the directory containing the json files] (default: {'./data/trump_tweet_data_archive-master/'})
+        output {str} -- [The path to where the output file will be written to] (default: {'data/raw_tweets.txt'})
+    """
+    for filename in os.listdir(directory):
+        if filename.endswith('.json'):
+            read_tweets(directory+filename, output)
+
